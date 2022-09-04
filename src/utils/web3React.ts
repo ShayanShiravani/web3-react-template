249f55faf70f0a6f2f7849e5c6b32ed93b9b6b9b
Shayan Shiravani <shayan5420@yahoo.com>
2022-09-04 12:52:59 +0430

    refactor: Refactor imports and add cross chain web3 provider

--- src/utils/web3React.ts
+++ src/utils/web3React.ts
@@ -1,5 +1,13 @@
+import { getAppRpcUrl } from "../configs/constants/rpc"
 import Web3 from "web3"
+import { random } from "lodash"
 
 export const getLibrary = (provider: any): Web3 => {
   return new Web3(provider)
+}
+
+export const getAppLibrary = (chainId: number): Web3 => {
+  const chainProviders = getAppRpcUrl(chainId)
+  const randomIndex = random(0, chainProviders.length - 1)
+  return new Web3(chainProviders[randomIndex])
 }
