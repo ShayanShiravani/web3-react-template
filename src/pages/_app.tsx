8e14617fdee9cc93a98fe727babd3e912f628250
Shayan Shiravani <shayan5420@yahoo.com>
2022-09-04 12:45:54 +0430

    refactor: Refactor imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,18 @@
 import type { AppProps } from 'next/app'
-import GlobalStyle from 'styles/Global'
+import GlobalStyle from '../styles/Global'
 import '../styles/globals.sass'
-import Providers from 'Providers'
-import { store } from 'state'
-import { MainLayout } from 'components/Layout/MainLayout'
+import Providers from '../Providers'
+import { store } from '../state/index'
+import { MainLayout } from '../components/Layout/MainLayout'
+import Web3ReactManager from '../components/Web3ReactManager/Index'
 
+import React from 'react'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import '@fontsource/montserrat'
-import Web3ReactManager from 'components/Web3ReactManager/Index'
+config.autoAddCss = false
 
 function MyApp(props: AppProps) {
-  config.autoAddCss = false
   return (
     <Providers store={store}>
       <GlobalStyle />
