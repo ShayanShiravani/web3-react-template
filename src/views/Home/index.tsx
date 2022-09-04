48151633137824f69afbcc037a1ae2e19fd78808
Shayan Shiravani <shayan5420@yahoo.com>
2022-09-04 12:54:27 +0430

    feat: Add directory structure and complete home page

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,17 +1,29 @@
+import Card from "components/Card";
 import Head from "next/head";
+import React from "react";
 
 const Home: React.FC = () => {
   return (
     <>
       <Head>
-        <title>DeBond</title>
+        <title>Web3 React Template</title>
       </Head>
-      <div>
-        <main>
-          <h2>
-            Welcome to playground
-          </h2>
-        </main>
+      <div className={'relative bg-blur bg-cover'}>
+        <picture>
+          <img className={'absolute -top-8'} src={'/images/common/blob-bg.svg'} alt="test" />
+        </picture>
+        <div className={'py-10 lg:py-20 px-4 sm:px-20 relative z-10'}>
+          <section className={'flex flex-col xl:flex-row items-center xl:items-start justify-evenly mb-24 lg:mb-36'}>
+            <section className="lg:w-3/5 w-full px-2 mb-2">
+              <Card>
+                <p>
+                When you decide to start a web3 project with react, you have to configure and create
+                many things from scratch.
+                </p>
+              </Card>
+            </section>
+          </section>
+        </div>
       </div>
     </>
   )
